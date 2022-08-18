9ade39d54dbdc0bf14e27acb4b0df72e344f6659
bjucs <bju@umd.edu>
2022-08-18 16:40:59 -0400

    Cleaned up UI, got working date label at top.

--- MacroTrack/ViewController.swift
+++ MacroTrack/ViewController.swift
@@ -8,10 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var date: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        date.text = getDate() 
+    }
+    
+    func getDate()-> String {
+        let date = Date()
+        let calendar = Calendar.current
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEEE"
+        let month = calendar.component(.month, from: date)
+        let day = calendar.component(.day, from: date)
+        let dayOfWeek = dateFormatter.string(from: date)
+        let thedate = dayOfWeek + ", " + String(month) + "/" + String(day)
+        return thedate
     }
 
 
